ac5b0a31d1b39c3e895a7b114d7d02e3fb605d75
Stoyan Ivanov <stoian_ivanov_ivanov@abv.bg>
2017-04-05 23:18:20 +0300

    added week7

---
+++ Week07/Vector.h
@@ -0,0 +1,49 @@
+//
+// Created by nickym on 4/5/17.
+//
+
+#ifndef INCLASS_VECTOR_H
+#define INCLASS_VECTOR_H
+
+
+class Vector {
+private:
+    int* contaner;
+    int size;
+    int capacity;
+
+    void resize(int newCapacity);
+public:
+    Vector();
+    Vector(int* arr, int size);
+    Vector(const Vector& other);
+    Vector& operator=(const Vector& other);
+    ~Vector();
+
+    int getSize();
+    int getCapacity();
+
+    bool isEmpty();
+
+    void sort(bool increasing);
+    bool isSorted(bool increasing);
+
+    int getAt(int index);
+    void setAt(int index, int element);
+
+    void add(int element);
+    void remove(int element);
+
+    Vector& operator+=(const Vector& other);
+    Vector& operator-=(const Vector& other);
+    Vector& operator*=(const Vector& other);
+    Vector& operator/=(const Vector& other);
+
+    friend Vector operator+(const Vector& v1, const Vector& v2);
+    friend Vector operator-(const Vector& v1, const Vector& v2);
+    friend Vector operator*(const Vector& v1, const Vector& v2);
+    friend Vector operator/(const Vector& v1, const Vector& v2);
+};
+
+
+#endif //INCLASS_VECTOR_H
